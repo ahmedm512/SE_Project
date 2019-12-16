82ff3e14426dd829a589ff80364592d3c209d2df
Ahmed Mohamed <1600155@eng.asu.edu.eg>
2019-12-16 10:04:46 +0200

    Starting project

---
+++ script.js
@@ -0,0 +1,11 @@
+// var firstName = document.getElementsByTagName("input")[0];
+// var button = document.getElementById("read");
+// var ul = document.getElementsByTagName("ul")[1];
+
+// button.addEventListener("click", function(){
+// 	var li = document.createElement("li");
+// 	li.appendChild(document.createTextNode(firstName.value));
+// 	ul.appendChild(li);
+// 	input.value = "";
+	
+// })
